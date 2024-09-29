aa99680158fd7f5b606c6239b9f89208209c9797
Mark Adhir Mondoll <mark@web3soft.ca>
2024-09-29 21:23:52 +0600

    CustomManager and User model is created

---
+++ profiles/backend.py
@@ -0,0 +1,20 @@
+from typing import Any
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user, get_user_model
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db.models import Q
+from django.http import HttpRequest
+
+User = get_user_model()
+
+class UsernameOrEmail(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(Q(user_name__iexact=username) | Q(email__iexact=username))
+        except User.DoesNotExist:
+            User().set_password(password)
+            return
+        except User.MultipleObjectsReturned:
+            user = User.objects.get(Q(user_name__iexact=username) | Q(email__iexact=username)).order_by('id').first()
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
--- profiles/models.py
+++ profiles/models.py
@@ -3,10 +3,63 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django. contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+
+class CustomManager(BaseUserManager):
+    def create_user(self, email, user_name, password, **extra_fields):
+        if not email:
+            raise ValueError('Email address is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, **extra_fields)
+        user.set_password(make_password(password))
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, user_name, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_verify', True)
+        extra_fields.setdefault('user_type', 'developer')
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('superuser must be is_staff=true')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('superuser must be is_superuser=true')
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('superuser must be is_active=true')
+        if extra_fields.get('is_verify') is not True:
+            raise ValueError('superuser must be is_verify=true')
+        
+        return self.create_user(email, user_name, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    USER_TYPE  = (
+        ('customer', 'customer'),
+        ('developer', 'developer')
+        )
+    email = models.EmailField(unique=True)
+    user_name = models.CharField(max_length=100, unique=True)
+    REQUIRED_FIELDS = ['user_name']
+    USERNAME_FIELD = 'email'
+    user_type = models.CharField(max_length=100, choices=USER_TYPE, default=USER_TYPE[0])
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_verify = models.BooleanField(default=False)
+
+    objects = CustomManager()
+
+    def __str__(self):
+        return str(self.email)
+
+    
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    username = models.CharField(max_length=100, blank=False, null=False)
     full_name = models.CharField(max_length=150, blank=True, null=True)
     address = models.TextField(max_length=300, blank=True, null=True)
     country = models.CharField(max_length=100, blank=True, null=True)
