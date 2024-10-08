042574820c6a544e69e051947fd258890b76de27
Mark Adhir Mondoll <mark@web3soft.ca>
2024-10-08 19:38:56 +0600

    Notification complete

--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('', include('store.urls')),
     path('order/', include('order.urls')),
     path('payment/', include('payment.urls')),
+    path('notification/', include('notification.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from notification.models import UserObj, Notification
 
-# Create your views here.
+def seenNotification(request, pk):
+    if request.user.is_authenticated:
+        user_obj = UserObj.objects.get(user=request.user)
+        notification_qs = Notification.objects.get(id=pk)
+        notification_qs.userobj.remove(user_obj)
+        notification_qs.is_read = True
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        return redirect('profiles:login')
